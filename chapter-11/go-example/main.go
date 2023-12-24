9726cc31f222e42cb85722b5cba1e6b888d65dc5
khaicm-0897 <chu.minh.khai@sun-asterisk.com>
2023-12-24 16:44:35 +0700

    Chapter 11 done

--- chapter-11/go-example/main.go
+++ chapter-11/go-example/main.go
@@ -1,7 +1,9 @@
 package main
 
 import "fmt"
+import "thisIsATest/room"
 
 func main() {
    fmt.Println("program started")
+   room.PrintDetails(112, 3, 2)
 }
--- chapter-11/go-example/room/room.go
+++ chapter-11/go-example/room/room.go
@@ -1,3 +1,8 @@
 package room
 
-const roomText = "%d : %d people / %d nights"
+import "fmt"
+
+// display information about a room
+func PrintDetails(roomNumber, size, nights int) {
+    fmt.Println(roomNumber, ":", size, "people /", nights, " nights ")
+}
